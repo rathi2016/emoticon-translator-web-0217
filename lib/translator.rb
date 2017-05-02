f17ebcaf6488ff10985c56bec759ce287c369ed0
Rathi <github email address>
2017-05-02 09:45:52 -0400

    learnt abt yml file

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,31 @@
 # require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(yml_file)
+emoticons = { "get_emoticon" => {}, "get_meaning"=>{} }
+YAML.load_file(yml_file).each do |meaning, arr|
+  emoticons["get_emoticon"][arr[0]] = arr[1]
+  emoticons["get_meaning"][arr[1]] = meaning
+end
+emoticons
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(yml_file,emoticon)
+  hash = load_library(yml_file)
+  if hash["get_emoticon"].has_key?(emoticon)
+     return hash["get_emoticon"][emoticon]
+   else
+     return "Sorry, that emoticon was not found"
+   end
+
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(yml_file,emoticon)
+  hash = load_library(yml_file)
+  if hash["get_meaning"].has_key?(emoticon)
+    return hash["get_meaning"][emoticon]
+  else
+    return "Sorry, that emoticon was not found"
+  end
+  
 end
